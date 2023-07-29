90300ce55aff64d5cc026cffe4ab3159b77c8d52
s.erfan.m <104711007+s-erfanm@users.noreply.github.com>
2023-07-29 11:38:38 +0330

    added custom error pages to flask app 404 , 500

--- main.py
+++ main.py
@@ -10,12 +10,25 @@ app = Flask(__name__)
 def index():
     creator_name = "s.erfan.m"
     foods = ["Pizza", "Felafel", "Pasta", "Kebab"]
+
     return render_template("index.html", creator_name=creator_name, all_foods=foods)
 
 # localhost:5000/user/John
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user=name)
+
+# CREATE custome error pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal sever error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
