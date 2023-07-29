dea82e2ac75438fa2b05249c6734888d33415ae1
s.erfan.m <104711007+s-erfanm@users.noreply.github.com>
2023-07-29 11:00:07 +0330

    first usable flask app for redering a hello world page

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route("/")
+def index():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
