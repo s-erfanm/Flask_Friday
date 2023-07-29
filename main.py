3855e42cbcd47d668eb17a2bf0562f3cc45899ba
s.erfan.m <104711007+s-erfanm@users.noreply.github.com>
2023-07-29 13:28:50 +0330

    in this pice of code i create a wtforms form and  a name input box in name.html page who sended data secure for main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, url_for
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "boss ... i had a wildlife accident"
+
+# CREATE A Form Class
+class SubmitForm(FlaskForm):
+    name = StringField("what is your name ? ", validators=[DataRequired()])
+    submit = SubmitField("submit")
+
 
 # Create a route decorator
 @app.route("/")
@@ -29,6 +38,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name page
+@app.route('/name/', methods=['POST','GET'])
+def name():
+    name = None
+    form = SubmitForm()
+    # Validate From
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",name=name, form=form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
