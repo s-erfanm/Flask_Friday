4c1edff50f2a0ff54cf897fd1874ccc5ec205328
s.erfan.m <104711007+s-erfanm@users.noreply.github.com>
2023-07-29 11:05:36 +0330

    how to recive a name or sth from the url

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return "<p>Hello, World!</p>"
 
+# localhost:5000/user/John
+@app.route('user/<name>')
+def user(name):
+    return f"<h1>Hello {name}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
